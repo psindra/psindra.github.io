d037ee77d72ec26b233d5c4926a7f7e89c3bd586
psindra <45101133+psindra@users.noreply.github.com>
2020-08-22 22:55:02 -0300

    Update adsY.js

--- YouTube/adsY/adsY.js
+++ YouTube/adsY/adsY.js
@@ -26,6 +26,3 @@ javascript: f = async function() {
 	var1 = await setTimeout(f,20);
 };
 a1 = setTimeout(f, 20);
-
-
-//javascript:f=function(){document.querySelector(".ytp-ad-skip-button")?document.querySelector(".ytp-ad-skip-button").click():document.querySelector(".ytp-ad-text")&&(document.querySelector(".html5-main-video").currentTime=document.querySelector(".html5-main-video").getDuration()+1),document.querySelector(".ytp-ad-overlay-close-button")&&document.querySelector(".ytp-ad-overlay-close-button").click(),document.querySelector(".ytp-ad-image-overlay")&&document.querySelector(".ytp-ad-image-overlay").remove(),document.querySelector("#confirm-button.style-scope.yt-confirm-dialog-renderer")&&(document.querySelector("#confirm-button.style-scope.yt-confirm-dialog-renderer").click(),document.querySelector("#confirm-button.style-scope.yt-confirm-dialog-renderer").remove()),document.querySelector("#player-ads")&&document.querySelector("#player-ads").remove()},a1=setInterval(f,20);
