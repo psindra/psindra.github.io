7620a8de57d8ea6ee23ca14ceb384047c90e5722
psindra <45101133+psindra@users.noreply.github.com>
2020-09-14 00:40:14 -0300

    Update adsY.js

--- YouTube/adsY/adsY.js
+++ YouTube/adsY/adsY.js
@@ -1,13 +1,13 @@
 // javascript: 
 f = async function() {
-	if (document.querySelector("#movie_player").classList.contains("ad-showing")) {
+	if (await document.querySelector("#movie_player").classList.contains("ad-showing")) {
 		var video_duration = await document.querySelector(".html5-main-video").getDuration();
 		await document.getElementsByTagName('video')[0].pause()
 		if (!document.querySelector("#movie_player").classList.contains("var1")){
 			await document.querySelector("#movie_player").classList.add("var1");
 			await alert('class "ad-showing"');
 		}	// fin de incorporación "var1"
-		await document.querySelector(".html5-main-video").currentTime = video_duration;
+		document.querySelector(".html5-main-video").currentTime = video_duration;
 	}
 	
     if (await document.querySelector(".ytp-ad-skip-button")) {
