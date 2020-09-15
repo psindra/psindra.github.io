85a6dd371cde39166139bfa1c676593f6a68b1f9
psindra <45101133+psindra@users.noreply.github.com>
2020-09-15 16:23:53 -0300

    Update YT_new_tab_best_format.js

--- YouTube/download_videos/YT_new_tab_best_format.js
+++ YouTube/download_videos/YT_new_tab_best_format.js
@@ -1,2 +1,2 @@
-JSON.parse( ytplayer.config.args.player_response).streamingData.formats.pop().url
+// JSON.parse( ytplayer.config.args.player_response).streamingData.formats.pop().url
 window.open(JSON.parse( ytplayer.config.args.player_response).streamingData.formats.pop().url)
