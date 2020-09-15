ccc1698f53cc86f3d8812742d7402207de5a6853
psindra <45101133+psindra@users.noreply.github.com>
2020-09-15 17:19:40 -0300

    Update YT_new_tab_best_format.js

--- YouTube/download_videos/YT_new_tab_best_format.js
+++ YouTube/download_videos/YT_new_tab_best_format.js
@@ -1,2 +1,13 @@
 // JSON.parse( ytplayer.config.args.player_response).streamingData.formats.pop().url
-window.open(JSON.parse( ytplayer.config.args.player_response).streamingData.formats.pop().url)
+// window.open(JSON.parse( ytplayer.config.args.player_response).streamingData.formats.pop().url)
+
+// video: 
+javascript:console.log("URL: " + JSON.stringify(JSON.parse( ytplayer.config.args.player_response).streamingData.formats.pop()).slice(JSON.stringify(JSON.parse( ytplayer.config.args.player_response).streamingData.formats.pop()).indexOf("http"),JSON.stringify(JSON.parse( ytplayer.config.args.player_response).streamingData.formats.pop()).lastIndexOf('","mimeType')
+ ));window.open(JSON.stringify(JSON.parse( ytplayer.config.args.player_response).streamingData.formats.pop()).slice(JSON.stringify(JSON.parse( ytplayer.config.args.player_response).streamingData.formats.pop()).indexOf("http"),JSON.stringify(JSON.parse( ytplayer.config.args.player_response).streamingData.formats.pop()).lastIndexOf('","mimeType'))
+ );
+
+
+// // audio:
+// javascript:console.log("URL: " + JSON.stringify(JSON.parse( ytplayer.config.args.player_response).streamingData.formats.pop()).slice(JSON.stringify(JSON.parse( ytplayer.config.args.player_response).streamingData.formats.pop()).indexOf("http"),JSON.stringify(JSON.parse( ytplayer.config.args.player_response).streamingData.formats.pop()).lastIndexOf('","mimeType')
+//  ));window.open(JSON.stringify(JSON.parse( ytplayer.config.args.player_response).streamingData.formats.pop()).slice(JSON.stringify(JSON.parse( ytplayer.config.args.player_response).streamingData.formats.pop()).indexOf("http"),JSON.stringify(JSON.parse( ytplayer.config.args.player_response).streamingData.formats.pop()).lastIndexOf('","mimeType'))
+//  );
