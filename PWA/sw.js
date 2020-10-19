cd3fefe0a9a80c0400875673ac2c89ce171502de
psindra <45101133+psindra@users.noreply.github.com>
2020-10-19 13:50:27 -0300

    Update sw.js

--- PWA/sw.js
+++ PWA/sw.js
@@ -1,5 +1,5 @@
 console.log("antes de instalar");
-self.addEventListener('install', ()=> {
+self.addEventListener('install', function(e) {{
     console.log("cuando se está instalando??");
     e.waitUntil(
         caches.open(cacheName).then(function(cache) {
