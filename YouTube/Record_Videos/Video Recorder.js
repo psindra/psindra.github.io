d8b302a3f0a20107825aa288a4c45d4e6263da8d
psindra <45101133+psindra@users.noreply.github.com>
2021-05-04 22:17:14 -0300

    Update Video Recorder.js

--- YouTube/Record_Videos/Video Recorder.js
+++ YouTube/Record_Videos/Video Recorder.js
@@ -17,19 +17,25 @@
          if (e.data.size > 0) {
              recordedChunks.push(e.data);
              console.info(e.data);
+             document.querySelector("#logo-icon > svg > g > g:nth-child(1) > polygon").setAttribute('fill', '#000000')
+             setTimeout(() => {
+                 document.querySelector("#logo-icon > svg > g > g:nth-child(1) > polygon").setAttribute('fill', '#FFFFFF')
+             }, 1000)
+
          }
          if (recording === false) {
              console.info("Send mediaRecorder.stop()");
              mediaRecorder.stop();
              recording = false;
-             console.info("Sended mediaRecorder.stop()");
          }
      };
      mediaRecorder.onstop = function() {
+         console.info("Sended mediaRecorder.stop()");
          const recordedBlob = new Blob(recordedChunks, {
              type: mimeType
          });
          recordedChunks = [];
+         document.querySelector("#logo-icon > svg > g > g:nth-child(1) > path").setAttribute('fill', '#FF0000')
          downloadLink = document.createElement('a');
          downloadLink.href = URL.createObjectURL(recordedBlob);
          downloadLink.target = '_blank';
@@ -37,4 +43,5 @@
      };
      mediaRecorder.start(5000);
      console.info("Recording..");
+     document.querySelector("#logo-icon > svg > g > g:nth-child(1) > path").setAttribute('fill', '#00FF00')
  }
