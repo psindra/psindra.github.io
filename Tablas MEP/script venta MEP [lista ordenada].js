e49c7a9e8a7d99ead78abe5003e026e851db6ff0
psindra <45101133+psindra@users.noreply.github.com>
2021-10-22 16:40:54 -0300

    Create script venta MEP [lista ordenada].js

---
+++ Tablas MEP/script venta MEP [lista ordenada].js
@@ -0,0 +1,47 @@
+bar = ""
+bar =fetch("https://www.bullmarketbrokers.com/Information/StockPrice/GetStockPrices?_ts=1634917153912&term=3&index=cedears")
+// bar =fetch("https://www.bullmarketbrokers.com/Information/StockPrice/GetStockPrices?term=3&index=cedears")
+// Contado Inmediato
+// https://www.bullmarketbrokers.com/Information/StockPrice/GetStockPrices?_ts=1634921905336&term=1&index=cedears&sortColumn=ticker&isAscending=true
+aa_json = await (await bar).json();
+ss = []
+aa_json.result.forEach(elemento=>{
+    ss.push(elemento.ticker);
+})
+
+ss_valida = []
+
+total = [[]]
+
+for (let i = 0; i < ss.length; i++) {
+//     total[i] = [ss[i],ss_valida[i]]
+    total[i] = [ss[i]]
+}
+
+let extracto = []
+for (let i = 0; i < ss.length; i++) {
+//     total[i][1] = (await(await fetch("https://www.bullmarketbrokers.com/Cotizaciones/Cedears/" + total[i][0] + "D")).status) == 200 ? '✅' : null
+    (await(await fetch("https://www.bullmarketbrokers.com/Cotizaciones/Cedears/" + total[i][0] + "D")).status) == 200 ? 
+    (total[i].push('✅') & extracto.push(ss[i]) ) : null
+
+}
+
+
+// ///////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// let i=0
+// aa_json.result.find(elem=>elem.ticker==extracto[i]).stockOffer.bidTop[0].price /
+// aa_json.result.find(elem=>elem.ticker==extracto[i]+"D").stockOffer.askTop[0].price
+
+lista_ordenada = []
+extracto.forEach(ticker=>{
+    cotizacion_resultante = aa_json.result.find(elem=>elem.ticker==ticker)?.stockOffer?.bidTop[0]?.price /
+                                    aa_json.result.find(elem=>elem.ticker==ticker+"D")?.stockOffer?.askTop[0]?.price;
+    console.info(ticker + " => " + cotizacion_resultante);
+    if(!isNaN(cotizacion_resultante)){
+        lista_ordenada.push([ticker, cotizacion_resultante])
+    }
+})
+lista_ordenada.sort((a,b)=>{return b[1]-a[1]});
+console.info(lista_ordenada);
