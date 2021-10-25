14537f65bd6028bb9377d03f813c8eb81c6f7f55
psindra <45101133+psindra@users.noreply.github.com>
2021-10-25 15:50:35 -0300

    Update script venta MEP [v2].js

--- Market/Tablas MEP/script venta MEP [v2].js
+++ Market/Tablas MEP/script venta MEP [v2].js
@@ -5,9 +5,9 @@
 // // https://www.bullmarketbrokers.com/Information/StockPrice/GetStockPrices?_ts=1634921905336&term=1&index=cedears&sortColumn=ticker&isAscending=true
 // aa_json = bar.json().then(resp=>{return resp});
 
- function async listarCEDEARS(){
+async function listarCEDEARS(){
     window.aa_json =''
-    await fetch("https://www.bullmarketbrokers.com/Information/StockPrice/GetStockPrices?_ts=1634917153912&term=3&index=cedears")
+    await (await fetch("https://www.bullmarketbrokers.com/Information/StockPrice/GetStockPrices?_ts=1634917153912&term=3&index=cedears"))
         .then(respuesta=>{return respuesta.json()}).then(respuesta=>{window.aa_json=respuesta})
     ss = []
     window.aa_json.result.forEach(elemento=>{
