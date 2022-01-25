0e9e81ee9d1036a95159833feb7a2a1fff17584e
pasartori <pasartori@indracompany.com>
2022-01-25 00:33:07 -0300

    funcionando

--- Screen Recorder/Media Capture and Streams API/src/index.js
+++ Screen Recorder/Media Capture and Streams API/src/index.js
@@ -1,14 +1,17 @@
 const startDOM= document.getElementById("start");
 const stopDOM = document.getElementById("stop");
 const videoDOM = document.querySelector("video");
-let recorder, stream;
+const playlistDOM = document.querySelector("#playlist");
+let recorder, stream, finalStreamBlob;
+ const chunks = [];
 
 async function startRecording() {
     audioStream = await navigator.mediaDevices.getUserMedia({audio:true});
 	screenStream = await navigator.mediaDevices.getDisplayMedia({
 		video:	{
-            mediaSource: "screen" ,
-            frameRate: { ideal: 10}
+            mediaSource: "screen"
+			/* ,
+            frameRate: { ideal: 10} */
         },
         audio:true
     });
@@ -20,7 +23,7 @@ async function startRecording() {
     
 
     const audioContext = new AudioContext();
-    _video = audioContext.createMediaStreamSource(video);
+    _video = audioContext?.createMediaStreamSource(video);
     _audio = audioContext.createMediaStreamSource(audio);
 
     combinedStream = audioContext.createMediaStreamDestination();
@@ -34,13 +37,16 @@ async function startRecording() {
     streamFinal.addTrack(screenStream.getTracks()[1]);
     streamFinal.addTrack(combinedStream.stream.getTracks()[0]);
 
-  recorder = new MediaRecorder(streamFinal);
+  recorder = new MediaRecorder(streamFinal, {mimeType:'video/webm;codecs=H264'});
+  console.log(recorder.mimeType);
 
-  const chunks = [];
+  /* const chunks = []; */
   recorder.ondataavailable = e => chunks.push(e.data);
   recorder.onstop = e => {
     const completeBlob = new Blob(chunks, { type: chunks[0].type });
     videoDOM.src = URL.createObjectURL(completeBlob);
+	/* playlistDOM.innerHTML += `<a href=${videoDOM.src}>${videoDOM.src}</a><button style="height:20px;font-size: 10px" onclick="videoDOM.src = ${videoDOM.src}">⬆</button><br>`; */
+	playlistDOM.innerHTML += `<a href=${videoDOM.src}>${videoDOM.src}</a><br>`;
   };
 
   recorder.start();
