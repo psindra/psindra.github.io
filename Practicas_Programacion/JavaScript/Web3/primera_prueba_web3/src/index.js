43ae772a1fb1ac500ba834e9a872c4d96fedeb20
pasartori <pasartori@indracompany.com>
2022-02-17 01:23:51 -0300

    segundo prueba versión web3

--- Practicas_Programacion/JavaScript/Web3/primera_prueba_web3/src/index.js
+++ Practicas_Programacion/JavaScript/Web3/primera_prueba_web3/src/index.js
@@ -6,28 +6,39 @@ let lbl_cake = document.querySelector("#lbl-cake")
 let span_cake = document.querySelector("#span-cake")
 
 
+if(!window.ethereum) {
+    alert("debes tener una wallet web3 compatible instalada. Ej:Metamask");
+    // throw new Error("=> Debes tener una wallet web3 compatible instalada. Ej:Metamask");
+    throw ("=> Debes tener una wallet web3 compatible instalada. Ej:Metamask");
+}
 
 var web3 = new Web3(window.ethereum);
+var contract_cake = new web3.eth.Contract(ABIjsonInterface, '0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82');
 
 btn_conectar.addEventListener('click', async()=>{
     var account;
-    alert("wallet => " + (web3.eth.defaultAccount = (await web3.eth.requestAccounts())[0]))
+    try {
+        alert("wallet => " + (web3.eth.defaultAccount = (await web3.eth.requestAccounts())[0]))
+    } catch (error) {
+        alert("debes permitir leer tu wallet");
+    }
     // alert("2=> " + await web3.eth.call({method: 'eth_requestAccounts'}))
     // debugger
     if(web3.eth.defaultAccount){
         span_address.textContent = web3.eth.defaultAccount;
     }
 
-    var contract_cake = new web3.eth.Contract(jsonInterface, '0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82');
 
     let cake_amount = await contract_cake.methods.balanceOf(web3.eth.defaultAccount)
-        .call({from: web3.eth.defaultAccount})
+        .call()
+        // .call({from: web3.eth.defaultAccount})
     lbl_cake.style.display = ''
     span_cake.textContent = (cake_amount/1e18) + '  Cake'
 })
 
 btn_sendTransaction.addEventListener('click', async ()=>{
     await web3.eth.sendTransaction({to: web3.eth.defaultAccount, value: 2e15})
+        .catch(()=>alert("Transacción rechazada "))
 })
 
 
