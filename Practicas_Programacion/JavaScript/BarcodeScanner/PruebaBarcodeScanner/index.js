73c53ca22f8bd3e76c56f8e9cfaf072a559db33f
polyys <pasartori@indracompany.com>
2023-05-29 21:23:32 -0300

    Primera Prueba BarcodeScanner

---
+++ Practicas_Programacion/JavaScript/BarcodeScanner/PruebaBarcodeScanner/index.js
@@ -0,0 +1,120 @@
+let cameraStream;
+let globalBarcodeDetector;
+const DECODER_TIMEOUT = 40;
+
+window.addEventListener("load", async ()=>{
+    await testBarcodeAPI();
+    await testCameraPermissions();
+    await populateUserMediaDevices();
+})
+
+async function testBarcodeAPI(){
+    if ('BarcodeDetector' in window) {
+        let formats = await window.BarcodeDetector.getSupportedFormats();
+        console.log({formats});
+        if (formats.length) {
+            //   barcodeDetectorUsable = true;
+            globalBarcodeDetector = new window.BarcodeDetector();
+            alert("Native Barcode API supported");
+        }
+    } else {
+        console.error("❌ Barcode Chrome API not");
+        alert("❌ Barcode Chrome API not");
+        // throw new Error("❌ Barcode Chrome API not");
+    }
+}
+
+async function testCameraPermissions(){
+    await navigator.mediaDevices.getUserMedia({video:true});
+}
+
+async function populateUserMediaDevices(){
+    navigator.mediaDevices.enumerateDevices()
+    .then(devices=>{
+        const inputDeviceSelect = document.querySelector("select#inputDeviceSelect");
+        inputDeviceSelect.replaceChildren();
+        const cameraDevices = devices.filter((device, index)=>{
+            if(device.kind == "videoinput"){
+                inputDeviceSelect.append(
+                    new Option(
+                        device.label ?? `Camara #${index}`,
+                        device.deviceId,
+                        device.label.toLocaleLowerCase().includes("back")
+                    )
+                );
+                return true;
+            } else {
+                return false;
+            }
+        });
+        if (!cameraDevices.length){
+            console.error("⛔ No cameras Detected")
+            alert("⛔ No cameras Detected")
+            // throw new Error("⛔ No cameras Detected")
+        }
+    })
+}
+
+function playCamera(){
+    stopCamera();
+    const inputDeviceSelect = document.querySelector("select#inputDeviceSelect");
+    const constraints = {
+        video: {deviceId: inputDeviceSelect.value},
+        audio: false
+    }
+    navigator.mediaDevices.getUserMedia(constraints).then( stream=>{
+        document.querySelector("video#camera").srcObject = stream;
+        cameraStream = stream;
+
+    }).catch(err=>{
+        console.error('getUserMediaError', err, err.stack);
+        alert('getUserMediaError', err, err.message);
+        // throw new Error("getUserMediaError", err)
+    })
+}
+
+function stopCamera(){
+    try{
+        if (cameraStream){
+            cameraStream.getTracks().forEach(track => track.stop());
+        }
+    } catch (e){
+        alert(e.message);
+    }
+}
+
+
+
+const playButton = document.querySelector("button#playButton");
+playButton.addEventListener("click", (ev)=>{
+    playCamera();
+    ev.target.disabled = true;
+    stopButton.disabled = false;
+})
+const stopButton = document.querySelector("button#stopButton");
+stopButton.addEventListener("click", (ev)=>{
+    stopCamera()
+    ev.target.disabled = true;
+    playButton.disabled = false;
+})
+
+
+const videoCamera = document.querySelector("video#camera");
+// let videoInterval;
+videoCamera.addEventListener("loadeddata", function startDecoding() {
+    // clearInterval(videoInterval);
+    // videoInterval = setInterval(decodeBarcode, 40);
+    setTimeout(decodeBarcode, DECODER_TIMEOUT);
+})
+
+function decodeBarcode(){
+    try {
+        globalBarcodeDetector.detect(videoCamera).then(detectedBarcode=>{
+            alert(JSON.stringify(detectedBarcode));
+            console.log(JSON.stringify(detectedBarcode));
+            setTimeout(decodeBarcode, DECODER_TIMEOUT);
+        })
+    } catch (err) {
+        alert("barcodeError", err.message);
+    }
+}
