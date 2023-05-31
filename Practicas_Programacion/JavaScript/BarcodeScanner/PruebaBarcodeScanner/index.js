41da19cd7a66b5a8a5a3fccdaa89ea778887450a
psindra <45101133+psindra@users.noreply.github.com>
2023-05-31 12:32:35 -0300

    Update index.js

--- Practicas_Programacion/JavaScript/BarcodeScanner/PruebaBarcodeScanner/index.js
+++ Practicas_Programacion/JavaScript/BarcodeScanner/PruebaBarcodeScanner/index.js
@@ -27,7 +27,7 @@ async function testBarcodeAPI(){
 
 async function testCameraPermissions(){
     return (navigator.mediaDevices.getUserMedia({video:true})
-    .then(stream=>{stream.getTracks().forEach(track => track.stop())));
+    .then(stream=>{stream.getTracks().forEach(track => await track.stop())});
 }
 
 async function populateUserMediaDevices(){
