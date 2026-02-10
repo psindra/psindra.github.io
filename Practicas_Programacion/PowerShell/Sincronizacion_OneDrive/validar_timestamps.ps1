367217049af16e88faeb8d226687558a6a2c5ec5
psindra <45101133+psindra@users.noreply.github.com>
2026-02-10 19:39:03 +0000

    en el mensaje final de validación muestra los erroneos y ahora también los correctos

--- Practicas_Programacion/PowerShell/Sincronizacion_OneDrive/validar_timestamps.ps1
+++ Practicas_Programacion/PowerShell/Sincronizacion_OneDrive/validar_timestamps.ps1
@@ -93,9 +93,5 @@ if ($validationErrors -eq 0) {
     Print_Log "Validación exitosa: marcas de tiempo correctas" -Level "SUCCESS"
 } else {
     Print_Log "Validación encontró $validationErrors discrepancias" -Level "WARNING"
-<<<<<<< Updated upstream
-    Print_Log "Y $validations elementos validados correctamente" -Level "INFO"
-=======
     Print_Log "y $validations elementos validados correctamente" -Level "INFO"
->>>>>>> Stashed changes
 }
