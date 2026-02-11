c21e35d8211d879ee1382b49f9003541452344ca
polyys <pasartori@indracompany.com>
2026-02-11 00:32:17 -0300

    Algún ejemplo de práctica de Service Workers que debo haber hecho y no commiteado

--- Practicas_Programacion/JavaScript/ServiceWorker/sw_1/index.js
+++ Practicas_Programacion/JavaScript/ServiceWorker/sw_1/index.js
@@ -1,8 +1,8 @@
-document.writeln("Up");
+console.log("ejecución index.js");
 
-document.writeln(navigator.serviceWorker)
+// document.writeln(navigator.serviceWorker)
 // console.log(navigator.serviceWorker)
 
-navigator.serviceWorker.register('sw.js');
+navigator.serviceWorker.register('sw_cacheWithNetworkFallbackAndNetworkUpdate.js');
 
-console.log(Web3);
+// console.log(Web3);
